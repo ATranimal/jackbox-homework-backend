7afaa5daa62d51262f8b522493a0fd35e9c8a4f3
atranimal <tran.andrew.a@gmail.com>
2021-02-21 13:56:51 -0500

    implementing stats calculations

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const { google } = require("googleapis");
+const cors = require("cors");
+const moment = require("moment");
 
 const app = express();
 const port = process.env.PORT;
 
 const API_KEY = process.env.API_KEY;
+
+app.use(cors());
 
 const youtube = google.youtube({
   version: "v3",
@@ -20,6 +24,7 @@ const youtube = google.youtube({
 app.get("/stats/:ID", (req, res) => {
   const youtubeID = req.params.ID;
 
+  const calculateStats = () => {};
   youtube.videos
     .list({
       id: youtubeID,
@@ -29,13 +34,54 @@ app.get("/stats/:ID", (req, res) => {
       const statistics = response.data.items[0].statistics;
       const contentDetails = response.data.items[0].contentDetails;
 
-      console.log(statistics);
-      console.log(contentDetails);
+      const duration = moment.duration(contentDetails.duration);
 
-      res.status(200).send({
-        statistics: statistics,
-        contentDetails: contentDetails,
-      });
+      const stats = {
+        level: 1,
+        formality: 0,
+        curiousity: 0,
+        creativity: 0,
+        compassion: 0,
+      };
+
+      // Block for stats calculations
+      statistics.viewCount < 25000
+        ? (stats.curiousity += 2)
+        : (stats.curiousity -= 1);
+
+      if (statistics.likeCount < statistics.dislikeCount) stats.compassion -= 2;
+
+      if (statistics.likeCount > statistics.viewCount / 100)
+        stats.compassion += 1;
+
+      if (statistics.dislikeCount > statistics.viewCount / 100)
+        stats.formality -= 1;
+
+      statistics.commentCount > statistics.viewCount / 1000
+        ? (stats.curiousty += 1)
+        : (stats.creativity -= 1);
+
+      if (statistics.favoriteCount > statistics.viewCount / 1000)
+        stats.formality += 1;
+
+      if (duration.hours > 0) formality += 1;
+      if (duration.hours === 0 && duration.minutes > 30) stats.creativity += 1;
+      if (
+        duration.hours == 0 &&
+        duration.minutes <= 30 &&
+        duration.minutes >= 5
+      )
+        commpassion += 1;
+      if (duration.hours === 0 && duration.minutes < 5) stats.curiousity += 1;
+
+      if (contentDetails.caption === "true") stats.formality += 2;
+      contentDetails.licensedContent === "true"
+        ? (stats.creativty -= 1)
+        : (stats.creativity += 1);
+
+      if (contentDetails.definition === "sd") stats.formality -= 1;
+
+      res.status(200).send(stats);
     })
     .catch((error) => {
       console.log(error);
